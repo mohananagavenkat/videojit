9ae634a217f1a4815f984f6483abeaa0ad46aad5
mohananagavenkat <mohananagavenkat@gmail.com>
2018-09-19 17:02:31 +0530

    Connected Application to mongodb and loaded Idea Schema

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-const express = require('express');
+const express = require("express");
 
-var exphbs  = require('express-handlebars');
+const exphbs = require("express-handlebars");
+
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// Connecting to mongodb
+mongoose
+  .connect(
+    "mongodb://localhost/videojit",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
-app.set('view engine', 'handlebars');
+// Loading Ideas Model
+
+require("./models/Ideas");
+
+const Idea = mongoose.model("IdeaModel");
+
+// Configuring the views
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
 
 const port = 5002;
 
-app.listen(port,() => {
-    console.log(`App is serving on http://localhost:${port}`);
-})
+// serving the application
+app.listen(port, () => {
+  console.log(`App is serving on http://localhost:${port}`);
+});
 
-
-app.get("/",(req,res)=>{
-    res.render('home');
+//Routes
+app.get("/", (req, res) => {
+  res.render("home");
 });
