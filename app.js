7579922bd62d888bf16cd27d575be3ce738c9c49
mohananagavenkat <mohananagavenkat@gmail.com>
2018-09-20 11:01:12 +0530

    Added method override package for the support of PUT, DELETE methods

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 
 const bodyParser = require("body-parser");
 
+const methodOverride = require("method-override");
+
 const app = express();
 
 // Connecting to mongodb
@@ -32,9 +34,11 @@ app.set("view engine", "handlebars");
 app.use(express.static("assets"));
 
 // bodyParser middle for parsing request body
-app.use(bodyParser.urlencoded({ extended: false })) // parse urlencoded data
-app.use(bodyParser.json()) // parse application/json data
+app.use(bodyParser.urlencoded({ extended: false })); // parse urlencoded data
+app.use(bodyParser.json()); // parse application/json data
 
+// Method override middleware to support PUT, DELETE HTTP methods
+app.use(methodOverride("_method"));
 
 //Routes
 app.get("/", (req, res) => {
@@ -45,79 +49,79 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/ideas",(req,res)=>{
-  Idea
-    .find({})
+app.get("/ideas", (req, res) => {
+  Idea.find({})
     .sort({ date: "desc" })
-    .then(ideas => { console.log(ideas); res.render("ideas", { ideas }) } )
-    .catch( err => console.log(err) )
+    .then(ideas => {
+      console.log(ideas);
+      res.render("ideas", { ideas });
+    })
+    .catch(err => console.log(err));
 });
 
-app.get("/ideas/edit/:id",(req,res)=>{
+app.get("/ideas/edit/:id", (req, res) => {
   // rendering edit form
-  Idea
-    .findOne({
-      _id:req.params.id
-    })
-    .then( idea => res.render("editIdea",{ idea }) )
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => res.render("editIdea", { idea }));
 });
 
-app.post("/ideas/edit/:id",(req,res)=>{
-  Idea
-    .findOne({
-      _id: req.params.id
-    })
-    .then(idea => {
-      idea.idea = req.body.idea;
-      idea.details = req.body.details;
-      idea.save()
-          .then( data => { console.log(data); res.redirect("/ideas") } )
-    })
-})
+app.post("/ideas/edit/:id", (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    idea.idea = req.body.idea;
+    idea.details = req.body.details;
+    idea.save().then(data => {
+      console.log(data);
+      res.redirect("/ideas");
+    });
+  });
+});
 
-app.get("/ideas/delete/:id",(req,res)=>{
+app.get("/ideas/delete/:id", (req, res) => {
   // Deleting Idea
-  Idea
-    .deleteOne({
-      _id: req.params.id
+  Idea.deleteOne({
+    _id: req.params.id
+  })
+    .then(data => {
+      console.log(data);
+      res.redirect("/ideas");
     })
-    .then(
-      data => {
-        console.log(data);
-        res.redirect("/ideas");
-      }
-    )
-    .catch(err => console.log(error) )
+    .catch(err => console.log(error));
 });
 
 app.get("/newidea", (req, res) => {
   res.render("newIdea");
-})
+});
 
 app.post("/newidea", (req, res) => {
   console.log(req.body);
   const errors = [];
-  const {idea,details} = req.body;
-  if(!idea){
-    errors.push({ message:"Please enter your idea" });
+  const { idea, details } = req.body;
+  if (!idea) {
+    errors.push({ message: "Please enter your idea" });
   }
-  if (!details){
+  if (!details) {
     errors.push({ message: "Please enter some brief details" });
   }
-  if(errors.length > 0){
-    res.render("newIdea",{
+  if (errors.length > 0) {
+    res.render("newIdea", {
       errors,
       idea,
       details
     });
-  }else{
-    const newIdea = new Idea({idea,details});
+  } else {
+    const newIdea = new Idea({ idea, details });
     newIdea
       .save()
-      .then( (data) => { console.log(data); res.redirect("/ideas") } )
-      .catch( (err) => console.log(err) )
+      .then(data => {
+        console.log(data);
+        res.redirect("/ideas");
+      })
+      .catch(err => console.log(err));
   }
-})
+});
 
 const port = 5002;
 
