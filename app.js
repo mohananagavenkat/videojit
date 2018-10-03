c57cd188337ef59858ce15042032a2bd03b1702a
mohananagavenkat <mohananagavenkat@gmail.com>
2018-10-03 15:16:17 +0530

    Added express session and connect-flash packages to show some flash messages and as well initialized passport and passport session so that it can serialize and store user data in req.user

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ const bodyParser = require("body-parser");
 
 const methodOverride = require("method-override");
 
+const passport = require("passport");
+
+const passportSetup = require("./config/passportSetup");
+
+const session = require("express-session");
+
+const flash = require("connect-flash");
+
 const app = express();
 
 // Connecting to mongodb
@@ -18,8 +26,6 @@ mongoose
   )
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.log(err));
-
-
 
 // Configuring the views
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -36,6 +42,34 @@ app.use(bodyParser.json()); // parse application/json data
 // Method override middleware to support PUT, DELETE HTTP methods
 app.use(methodOverride("_method"));
 
+// configuring express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// configuring flash
+app.use(flash());
+
+//initializing passport and passport-session
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// setting up some global variables which will be there for each request
+app.use((req, res, next) => {
+  // console.log(req.method,req.url);
+  // console.log(req.flash("error"));
+  res.locals.success_message = req.flash("success_message");
+  res.locals.error_message = req.flash("error_message");
+  res.locals.error = req.flash("error");
+  res.locals.user = req.user;
+  next();
+});
+
 //Loading Routes
 
 const ideaRoutes = require("./routes/ideas");
@@ -44,7 +78,6 @@ const generalRoutes = require("./routes/general");
 
 const authRoutes = require("./routes/auth");
 
-
 // using loaded routes
 
 app.use(ideaRoutes);
@@ -52,7 +85,6 @@ app.use(ideaRoutes);
 app.use(generalRoutes);
 
 app.use(authRoutes);
-
 
 const port = 5002;
 
