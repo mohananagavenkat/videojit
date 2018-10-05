a11e9365b20491892a8afbfcf2d90cf1548039b0
mohananagavenkat <mohananagavenkat@gmail.com>
2018-10-05 20:52:37 +0530

    Sending an email with activation link when user signedup, and activating user when user clicked on that activation link, after successful verification sending welcome email to the user

--- routes/auth.js
+++ routes/auth.js
@@ -12,6 +12,12 @@ const passport = require("passport");
 
 const { isAlreadyLoggedIn } = require("../helpers/authHelper");
 
+const VerifyToken = require("../models/VerifyToken");
+
+const crypto = require("crypto");
+
+const mailer = require("../config/nodeMailerSetup");
+
 router.get("/login", isAlreadyLoggedIn, (req, res) => {
   res.render("login");
 });
@@ -20,11 +26,14 @@ router.get("/signup", isAlreadyLoggedIn, (req, res) => {
   res.render("signup");
 });
 
-router.post("/login", passport.authenticate('local', {
-    successRedirect: '/ideas',
-    failureRedirect: '/login',
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/ideas",
+    failureRedirect: "/login",
     failureFlash: true
-}));
+  })
+);
 
 router.post("/signup", (req, res) => {
   const errors = [];
@@ -33,11 +42,16 @@ router.post("/signup", (req, res) => {
     errors.push({ message: "Password should be same in both fields" });
   if (password.length < 8)
     errors.push({ message: "Password should be minimum of 8 letters" });
+  if (errors.length > 0) {
+    res.render("signup", { name, email, mobile, errors });
+    return;
+  }
   User.findOne({ email: email })
     .then(user => {
       if (user) {
-        errors.push({ mecssage: "Email already exist" });
+        errors.push({ message: "Email already exist" });
         res.render("signup", { errors, name, email, password, mobile });
+        return;
       } else {
         const newUser = new User({
           name,
@@ -55,9 +69,35 @@ router.post("/signup", (req, res) => {
             newUser.password = hash;
             newUser
               .save()
-              .then(() => {
+              .then(user => {
                 console.log("User saved successfully");
-                res.redirect("/login");
+                const token = crypto.randomBytes(64).toString("hex");
+                const url = ``;
+                const newVerifyToken = new VerifyToken({
+                  _userId: user.id,
+                  token: token
+                });
+                newVerifyToken.save().then(() => {
+                  let mailOptions = {
+                    from: '"videojit 👻" <noreply@videojit.com>', // sender address
+                    to: user.email, // list of receivers
+                    subject: "Activation Link ✔", // Subject line
+                    html: `<h3>Welcome to videojit 👻</h3>
+                    <p>please click on the link to activate your account <a href="http://localhost:5002/activate_user/${token}">Click Here</a></p>`
+                  };
+                  mailer.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                      return console.log(error);
+                    }
+                    console.log(
+                      "Message sent successfully 👍. MessageId : %s",
+                      info.messageId
+                    );
+                    res.render("notifying_about_activation_link", {
+                      url: `http://localhost:5002/resend_verify_email/${newVerifyToken.id}`
+                    });
+                  });
+                });
               })
               .catch(err => console.log);
           });
@@ -67,10 +107,75 @@ router.post("/signup", (req, res) => {
     .catch(err => console.log);
 });
 
-router.get("/logout",(req,res)=>{
-    req.logout();
-    req.flash("success_message","Logged out successfully");
-    res.redirect("/login");
-})
+router.get("/notifying_about_activation_link", (req, res) => {
+  res.render("notifying_about_activation_link");
+});
+
+router.get("/activate_user/:token", (req, res) => {
+  const token = req.params.token;
+  VerifyToken.findOne({ token })
+    .then(record => {
+      if (!record) {
+        req.flash(
+          "error_message",
+          "Somethig went wrong! Please try again later"
+        );
+        res.redirect("/login");
+        return;
+      }
+      console.log(record);
+      const userId = record._userId;
+      User.findById(userId).then(user => {
+        if (!user) {
+          req.flash(
+            "error_message",
+            "Somethig went wrong! Please try again later"
+          );
+          res.redirect("/login");
+          return;
+        }
+        else if(user.isVerified == true){
+          req.flash(
+            "success_message",
+            "Your account has already been verified 😃"
+          );
+          res.redirect("/login");
+          return;
+        }
+        user.isVerified = true;
+        user.save().then(() => {
+          console.log("user verified successfully");
+          let mailOptions = {
+            from: '"videojit 👻" <noreply@videojit.com>', // sender address
+            to: user.email, // list of receivers
+            subject: "Welcome Email 😍 ", // Subject line
+            html: `<h3>Welcome to videojit 👻</h3>
+                    <p>Here you can store your Ideas of future 😃</p>`
+          };
+          mailer.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              return console.log(error);
+            }
+            console.log(
+              "Message sent successfully 👍. MessageId : %s",
+              info.messageId
+            );
+            req.flash(
+              "success_message",
+              "Account has been verified successfully 😃"
+            );
+            res.redirect("/login");
+          });
+        });
+      });
+    })
+    .catch(err => console.log(err));
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_message", "Logged out successfully");
+  res.redirect("/login");
+});
 
 module.exports = router;
